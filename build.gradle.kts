eb8af84e8772e329478c6140ca21353cd7dff4f5
The0x539 <the0x539@gmail.com>
2021-08-21 16:14:42 -0500

    better muxing? torrent files?

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,30 +13,44 @@ plugins {
 subs {
 	readProperties("sub.properties")
 	episodes(getList("episodes"))
+	batches(getMap("seasons", "episodes"))
 
-	fun Task.splitEpNum(): Pair<String, String> {
-		val (season, ep) = episode.split("x")
-		return Pair(season, ep)
+	fun Task.getSeason(): String {
+		return episode.substringBefore('x')
+	}
+
+	fun Task.getBatchSeason(): String {
+		return batch.substringAfter("book")
+	}
+
+	fun Task.getEp(): String {
+		return episode.substringAfter('x')
+	}
+
+	fun Task.formatEpisode(tags: String): String {
+		return "Infinity Train S0${getSeason()}E${getEp()} - ${getRaw("title")} (${tags}) [0x539]"
+	}
+
+	fun Task.formatSeason(tags: String): String {
+		return "Infinity Train Book ${getRaw("numeral")} - ${getRaw("title")} (S0${getBatchSeason()}) ($tags) [0x539]"
 	}
 
 	val mks by task<Mux> {
-		val (season, ep) = splitEpNum()
+		from("${getSeason()}/${getEp()}.ass")
 
-		from("$season/$ep.ass")
-
-		attach("fonts", "fonts/$episode") {
+		attach("fonts", "fonts/${episode}") {
 			includeExtensions("ttf", "otf")
 		}
 
 		onFaux(ErrorMode.FAIL)
 
-		out("$season/mux/$ep.mks")
+		val name = formatEpisode("SUBS")
+		out("${getSeason()}/mux/${name}.mks")
 	}
 
 	val mkv by task<Mux> {
-		val (season, ep) = splitEpNum()
-
-		from("$season/raw/$ep.mkv") {
+		val season = getSeason()
+		from("${getSeason()}/raw/${getEp()}.mkv") {
 			tracks {
 				include(track.type != TrackType.SUBTITLES)
 			}
@@ -44,6 +58,23 @@ subs {
 
 		from(mks.item())
 
-		out("$season/mux/$ep.mkv")
+		val name = formatEpisode(getRaw("book${getSeason()}.src") + " 1080p")
+		out("${getSeason()}/mux/${name}.mkv")
+	}
+
+	batchtasks {
+		val mks_torrent by task<Torrent> {
+			from(mks.batchItems())
+			val name = formatSeason("SUBS")
+			into(name)
+			out(name + ".torrent")
+		}
+
+		val mkv_torrent by task<Torrent> {
+			from(mkv.batchItems())
+			val name = formatSeason(getRaw("src") + " 1080p")
+			into(name)
+			out(name + ".torrent")
+		}
 	}
 }
